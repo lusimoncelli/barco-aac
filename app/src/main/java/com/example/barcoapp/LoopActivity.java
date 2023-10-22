3908a247744982a6574730847fd07d5f421a568e
milalangone <mlangone@itba.edu.ar>
2023-10-22 18:39:17 -0300

    added delete button

--- app/src/main/java/com/example/barcoapp/LoopActivity.java
+++ app/src/main/java/com/example/barcoapp/LoopActivity.java
@@ -2,6 +2,7 @@ package com.example.barcoapp;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,9 +11,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoopActivity extends AppCompatActivity {
 
-    private int layoutId ;
+    private int layoutId;
     private EditText enteredText;
     private Handler handler = new Handler();
+    private Handler longPressHandler = new Handler();
+    private boolean isLongPressing = false;
 
     // Button initialization
     private Integer[] buttonsId;
@@ -22,14 +25,14 @@ public class LoopActivity extends AppCompatActivity {
     private int currentButtonIndex = 0;
     private boolean loopRunning = false;
 
-    protected LoopActivity(Integer[] buttonsId, int layoutId){
+    protected LoopActivity(Integer[] buttonsId, int layoutId) {
         this.buttonsId = buttonsId;
         this.buttons = new Button[buttonsId.length];
         this.layoutId = layoutId;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(this.layoutId);
 
@@ -38,30 +41,42 @@ public class LoopActivity extends AppCompatActivity {
         enteredText.setVisibility(View.VISIBLE);
 
         int index = 0;
-        for(Integer buttonId : buttonsId)
+        for (Integer buttonId : buttonsId)
             this.buttons[index++] = findViewById(buttonId);
-
 
         for (Button button : buttons) {
             button.setVisibility(View.INVISIBLE);
+            button.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        isLongPressing = true;
+                        longPressHandler.postDelayed(longPressRunnable, 2000);
+                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                        isLongPressing = false;
+                        longPressHandler.removeCallbacks(longPressRunnable);
+                    }
+                    return false;
+                }
+            });
         }
 
         startButtonLoop();
         startLoop();
     }
 
-    private void setButtonVisibility(int index, int visibility){
-        if (index >= 0 && index < buttons.length){
+    private void setButtonVisibility(int index, int visibility) {
+        if (index >= 0 && index < buttons.length) {
             buttons[index].setVisibility(visibility);
         }
     }
 
-    private void startButtonLoop(){
+    private void startButtonLoop() {
         loopRunning = true;
         setButtonVisibility(currentButtonIndex, View.VISIBLE);
     }
 
-    private void startLoop(){
+    private void startLoop() {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -69,18 +84,16 @@ public class LoopActivity extends AppCompatActivity {
                 currentButtonIndex = (currentButtonIndex + 1) % buttons.length;
                 setButtonVisibility(currentButtonIndex, View.VISIBLE);
 
-                if (loopRunning){
+                if (loopRunning) {
                     handler.postDelayed(this, 1000);
                 }
             }
         }, 1000);
     }
 
-
     private void stopButtonLoop() {
         loopRunning = false;
         setButtonVisibility(currentButtonIndex, View.INVISIBLE);
-        //currentButtonIndex = 0;
     }
 
     private void appendText(String text) {
@@ -89,16 +102,24 @@ public class LoopActivity extends AppCompatActivity {
         startButtonLoop();
     }
 
-    public Button getButton(){
+    public Button getButton() {
         return buttons[currentButtonIndex];
     }
 
-    public void onButtonClick(View view){
-
-        if (loopRunning){
+    public void onButtonClick(View view) {
+        if (loopRunning) {
             Button clickedButton = (Button) view;
             String buttonText = clickedButton.getText().toString();
             appendText(buttonText);
         }
     }
+
+    private Runnable longPressRunnable = new Runnable() {
+        @Override
+        public void run() {
+            enteredText.setText("");
+            stopButtonLoop();
+            startButtonLoop();
+        }
+    };
 }
